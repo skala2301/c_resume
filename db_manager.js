e4ac938ca055fa60c534310f8829c81f9812eb22
skala2301 <krlos_2301@hotmail.com>
2023-08-29 02:34:49 -0600

    Update db_manager.js
    Elements are pushed to database instead of writing everything at once, When calling onValue the new function passed adds all the elements to the innerHtml one by one through a for loop

--- db_manager.js
+++ db_manager.js
@@ -1,15 +1,29 @@
+
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import { getDatabase, ref } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import { getDatabase, ref, set, push, onValue, get } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+export var comments=null
 
 const appSettings={
   databaseURL:"https://first-project-75872-default-rtdb.firebaseio.com/"
 }
 
-const fireb_app=initializeApp(appSettings)
+const fireb_app=initializeApp(appSettings);
 const firebase_db=getDatabase(fireb_app);
-const comments_db=ref(firebase_db, "comments")
+const ref_db=ref(firebase_db, "comments");
 
-console.log(fireb_app)
-console.log(firebase_db)
-console.log(comments_db)
 
+export function writetodb(data){
+    push(ref_db, data)
+}
+
+
+onValue(ref_db,function(snapshot){
+    if (snapshot.exists()) {
+        document.getElementById("all_recommendations").innerHTML="";
+        for(const element in snapshot.val()) {
+            document.getElementById("all_recommendations").innerHTML+=snapshot.val()[element];
+        }
+      } else {
+        document.getElementById("all_recommendations").innerHTML= "";
+      }
+})
