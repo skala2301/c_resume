f23d01cd4ff1940e3c0052a0463426caaf7b49c4
skala2301 <krlos_2301@hotmail.com>
2023-08-29 03:44:25 -0600

    Update script.js
    simplified the code and added comments

--- script.js
+++ script.js
@@ -1,84 +1,44 @@
-
+import * as my_db from "./my_module.mjs"
+//load elements to be used from the html doc
 var btnAddRecommendation=document.getElementById("recommend_btn")
 var btnConfirm=document.getElementById("confirm_btn")
 var btnCancel=document.getElementById("cancel_btn")
 
-function load_comments(){
-  var my_comments=JSON.parse(localStorage.getItem("my_comments"));
-  if(my_comments!=null){
-    document.getElementById("all_recommendations").innerHTML=my_comments;
-    console.log("added ", my_comments);
-  } else {
-    console.log("No Comments");
-  } 
-}
+//Add all event listeners
+btnAddRecommendation.addEventListener("click", function(){showPopup(true)})//the submit button when clicked calls showPopup() function
+btnConfirm.addEventListener("click", function(){addRecommendation(true)})//part of the Popup shown for confirmation, when pushed it adds the new comment if there is any
+btnCancel.addEventListener("click", function(){addRecommendation(false)})//part of the Popup shown for confirmation, when pushed it closes the popup and cancel the comment submit
 
-
-
-function addRecommendation() {
-  // Get the message of the new recommendation
-  showPopup(true)
-
-}
-/*
-function showPopup(bool) {
-  if (bool) {
+//Shows the Popup window
+function showPopup(active) {
+  if (active) {//when active is true it disable the submit button and shows the popup window
     document.getElementById('recommend_btn').disabled=true
     document.getElementById('popup').style.visibility = 'visible'
-  } else {
+  } else {//if active false enables the submit button and hide the popup window
     document.getElementById('recommend_btn').disabled=false
     document.getElementById('popup').style.visibility = 'hidden'
   }
 }
-*/
 
 
-
-function showPopup(bool, proceed=false) {
-  if (bool) {
-    document.getElementById('recommend_btn').disabled=true
-    document.getElementById('popup').style.visibility = 'visible'
-  } else {
-    document.getElementById('recommend_btn').disabled=false
-    document.getElementById('popup').style.visibility = 'hidden'
-  }
-  if(proceed){
-    let recommendation_title=document.getElementById("new_recommendation_title").value;
-    let recommendation = document.getElementById("new_recommendation").value;
-    if(recommendation_title!=""){
-      recommendation_title="<b>"+recommendation_title+":</b><br/>"
+//It is called when btnConfirm or btnCancel are clicked
+function addRecommendation(proceed=false) {
+  showPopup(false);//closes the popup window and enable submit again
+  if(proceed){//if user press confirm button, the program proceeds to add the new comment to the database
+    let recommendation = document.getElementById("new_recommendation").value;//get the body of the new recommendation comment
+    if (recommendation != null && recommendation.trim() != ""){ //check if there is any comment into recommendation
+      let new_comment_element=StructComment(recommendation)//new recommendation is structured with the string retrived by StructComment() function
+      my_db.writetodb(new_comment_element);//write new data into database
     }
-    // If the user has left a recommendation, display a pop-up
-    let new_comment_element="\<div class='recommendation'\>"+recommendation_title+"\<span\>&#8220;\</span\>"+recommendation+"\<span\>&#8221;\</span\>\</div\>";
-    if (recommendation != null && recommendation.trim() != "") {
-      console.log("New recommendation added");
-      
-      var my_comments=JSON.parse(localStorage.getItem("my_comments"));
-      if(my_comments!=null){
-        my_comments+=new_comment_element;
-        console.log("added ", my_comments);
-      } else {
-        my_comments=new_comment_element;
-        console.log("Elemento nulo");
-      } 
-      
-      document.getElementById("all_recommendations").innerHTML=my_comments;
-      localStorage.setItem("my_comments",JSON.stringify(my_comments));
-      recommendation = "";
-    }
-  }else{
-    return false
   }
 }
 
-function readtextFile(){
-
+//This function structures the string with the html format so it may be added as a new comment
+function StructComment(recommendation){
+  let recommendation_title=document.getElementById("new_recommendation_title").value;//Get the value from the tittle
+  
+  if(recommendation_title!=""){//check if the was any title entered
+    recommendation_title="\<b\>"+recommendation_title+":\</b\>\<br/\>"//if a title was entered we provide the correct format to it
+  }
+  return "\<div class='recommendation'\>"+recommendation_title+"\<span>&#8220;\</span\>"+recommendation+"\<span>&#8221;\</span\>\</div\>";//the new string is sent to use as the full comment
 }
-
-document.addEventListener("DOMContentLoaded",load_comments)
-btnConfirm.addEventListener("click", function(){showPopup(false,true)})
-btnCancel.addEventListener("click", function(){showPopup(false,false)})
-btnAddRecommendation.addEventListener("click", function(){addRecommendation()})
-
-
-
